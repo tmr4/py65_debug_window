cf40966592e6efa91a956c63747b52df33a949bc
tmr4 <42870032+tmr4@users.noreply.github.com>
2022-01-01 08:11:58 -0800

    Create interrupts.py

---
+++ interrupts.py
@@ -0,0 +1,9 @@
+from devices.via65c22 import VIA
+from devices.acia65c51 import ACIA
+
+class Interrupts:
+    def __init__(self, mon, mpu):
+        mpu.IRQ_pin = 1
+        self.via = VIA(0x8800, mpu)
+        ACIA(0x8400, mpu, mon)
+        
